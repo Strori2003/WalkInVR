4b9de5cf98b7837106e3a9a5e454e19f5e106a9c
Strori2003 <98840851+Strori2003@users.noreply.github.com>
2025-11-24 18:48:55 +0300

    sevo 1st script

---
+++ Arduino/Servo/Servo/Servo.ino
@@ -0,0 +1,18 @@
+#include <Servo.h> // подключаем библиотеку для работы с сервоприводом
+
+Servo servo1; // объявляем переменную servo типа "servo1"
+int pos = 0;
+int speed;
+
+void setup() {
+   servo1.attach(10); // 
+   pos = map(speed, -100,100,0,180);
+   servo1.write(pos); // number 0-180 for motor SPEED
+   //0: full speed clockwise
+   //90: stopped
+   //180: full speed counter-clockwise
+}
+
+void loop() {
+   servo1.write(0);
+}
