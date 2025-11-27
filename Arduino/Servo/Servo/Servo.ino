a2e46ca753bc7cb6f265ae1a9a366087af1d8322
Strori2003 <98840851+Strori2003@users.noreply.github.com>
2025-11-27 17:47:41 +0300

    double servo

--- Arduino/Servo/Servo/Servo.ino
+++ Arduino/Servo/Servo/Servo.ino
@@ -1,13 +1,18 @@
 #include <Servo.h> // подключаем библиотеку для работы с сервоприводом
 
 Servo servo1; // объявляем переменную servo типа "servo1"
+Servo servo2; 
 int pos = 0;
 int speed;
 
 void setup() {
    servo1.attach(10); // 
-   pos = map(speed, -100,100,0,180);
+      servo2.attach(11); // 
+
+  // pos = map(speed, -100,100,0,180);
    servo1.write(pos); // number 0-180 for motor SPEED
+      servo2.write(pos); // number 0-180 for motor SPEED
+
    //0: full speed clockwise
    //90: stopped
    //180: full speed counter-clockwise
@@ -15,4 +20,6 @@ void setup() {
 
 void loop() {
    servo1.write(0);
+      servo2.write(180);
+
 }
